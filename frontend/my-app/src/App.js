0ba8e8a1a6de52a8ba0043e9bda9c13c5ca54b53
Sahil1157 <143487205+sahil1157@users.noreply.github.com>
2024-10-22 09:29:23 +0545

    Converted my-app from a submodule to a regular folder

---
+++ frontend/my-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react'
+
+const socket = io("http://localhost:5000");
+
+function App() {
+
+  return (
+    <div>
+      hey there
+    </div>
+  )
+}
+
+export default App
